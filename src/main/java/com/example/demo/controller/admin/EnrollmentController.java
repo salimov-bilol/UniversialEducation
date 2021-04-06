d0d2d0940d0da666b4ada509a35778ebfb403bee
salimov-bilol <salimovb1999@gmail.com>
2021-04-06 09:45:01 +0500

    Search added in Enrollment

--- src/main/java/com/example/demo/controller/admin/EnrollmentController.java
+++ src/main/java/com/example/demo/controller/admin/EnrollmentController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Enrollment;
 import com.example.demo.exception.StudentAlreadyEnrolledException;
 import com.example.demo.service.CourseService;
 import com.example.demo.service.EnrollmentService;
+import com.example.demo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,18 +18,20 @@ import java.util.Map;
 public class EnrollmentController {
 
     private final CourseService courseService;
-
     private final EnrollmentService enrollmentService;
+    private final UserService userService;
 
-    public EnrollmentController(CourseService courseService, EnrollmentService enrollmentService) {
+    public EnrollmentController(CourseService courseService, EnrollmentService enrollmentService, UserService userService) {
         this.courseService = courseService;
         this.enrollmentService = enrollmentService;
+        this.userService = userService;
     }
 
     @GetMapping("/enrollment")
     public String enrollment(Model model) {
         model.addAttribute("courses", this.courseService.findAll());
         model.addAttribute("enrollment", new Enrollment());
+        model.addAttribute("users", this.userService.findStudents());
         return "enrollment";
     }
 
